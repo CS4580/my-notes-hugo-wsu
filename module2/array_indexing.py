45121bf3920ba9cf35894e7dcec0448cb88e06e4
Hugo Valle <hugo.valle@gmail.com>
2024-09-11 08:40:58 -0600

    Update my Numpy  notes

---
+++ module2/array_indexing.py
@@ -0,0 +1,30 @@
+"""Array indexing
+"""
+import numpy as np
+
+def main():
+    """Driven Function
+    """
+    arr_1d = np.arange(10)
+    # Access the second element. Index notation
+    print(arr_1d[1])
+    # Last element
+    print(arr_1d[-1])
+    # Access 2D array elements
+    arr_2d = np.array([[21, 22, 23, 24],
+             [31, 32, 33, 34],
+             [41, 42, 43, 44]])
+    print(f'The 0,0 element {arr_2d[0,0]}') 
+    print(f'The 2,-2 element {arr_2d[2,-2]}') 
+    print(f'Full first row {arr_2d[0]}')
+    
+    # Slicing
+    arr_1d = np.arange(10)
+    print(f'All elements {arr_1d}')
+    print(f'Slicing elements 1:4 {arr_1d[1:4]}')
+    print(f'Slicing elements 1:4:2 {arr_1d[1:4:2]}')
+
+
+
+if __name__ == '__main__':
+    main()
